693c4bf7c70d64dde73a6ebcf57ebc9d09d131ca
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-12-12 09:20:22 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,52 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        control.reset()
         GREEN()
-    } else {
-        control.reset()
+    } else if (receivedNumber == 2) {
         RED()
     }
 })
 function Sensor () {
-	
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+}
+function phantom () {
+    GREEN()
+    basic.pause(5000)
+    for (let index = 0; index < 2; index++) {
+        YELLOW()
+        basic.pause(2000)
+    }
+    basic.pause(200)
+    for (let index = 0; index < 10; index++) {
+        RED()
+        basic.pause(1000)
+    }
+    for (let index = 0; index < 10; index++) {
+        basic.showString("" + (pedestrian))
+        pedestrian += -1
+    }
+    for (let index = 0; index < 5; index++) {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+}
+function trafficlightreal2 () {
+    for (let index = 0; index < 5; index++) {
+        GREEN()
+        basic.pause(1000)
+    }
+    for (let index = 0; index < 5; index++) {
+        RED()
+        basic.pause(1000)
+    }
+    for (let index = 0; index < 2; index++) {
+        YELLOW()
+        basic.pause(2000)
+    }
 }
 input.onButtonPressed(Button.A, function () {
     RED()
@@ -33,6 +71,7 @@ input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 10; index++) {
         basic.showString("" + (pedestrian))
         pedestrian += -1
+        basic.pause(1000)
     }
     for (let index = 0; index < 5; index++) {
         basic.showIcon(IconNames.No)
@@ -71,8 +110,8 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     RED()
     basic.pause(5000)
+    music.playMelody("C5 C5 C5 C5 A A B B ", 500)
     for (let index = 0; index < 10; index++) {
-        music.playMelody("C5 C5 C5 C5 A A B B ", 500)
         basic.showLeds(`
             # . # . .
             # # # # #
@@ -93,6 +132,7 @@ input.onButtonPressed(Button.B, function () {
         music.playMelody("- C5 C5 C5 - C5 C5 C5 ", 500)
         basic.showString("" + (pedestrian))
         pedestrian += -1
+        basic.pause(1000)
     }
     for (let index = 0; index < 5; index++) {
         basic.showIcon(IconNames.No)
@@ -110,15 +150,7 @@ input.onButtonPressed(Button.B, function () {
     RED()
 })
 function trafficlightreal () {
-    GREEN()
-    basic.pause(5000)
-    for (let index = 0; index < 2; index++) {
-        YELLOW()
-        basic.pause(2000)
-    }
-    basic.pause(200)
     RED()
-    basic.pause(5000)
     for (let index = 0; index < 10; index++) {
         basic.showLeds(`
             # . # . .
@@ -139,11 +171,22 @@ function trafficlightreal () {
     for (let index = 0; index < 10; index++) {
         basic.showString("" + (pedestrian))
         pedestrian += -1
+        basic.pause(1000)
     }
     for (let index = 0; index < 5; index++) {
         basic.showIcon(IconNames.No)
     }
     basic.pause(2000)
+    for (let index = 0; index < 5; index++) {
+        GREEN()
+        basic.pause(5000)
+    }
+    for (let index = 0; index < 2; index++) {
+        YELLOW()
+        basic.pause(2000)
+    }
+    basic.pause(200)
+    RED()
 }
 function YELLOW () {
     range = strip.range(0, 1)
@@ -153,8 +196,8 @@ function YELLOW () {
     range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
-let distance = 0
 let range: neopixel.Strip = null
+let distance = 0
 let pedestrian = 0
 let strip: neopixel.Strip = null
 basic.showIcon(IconNames.No)
@@ -167,15 +210,10 @@ pedestrian = 10
 let yellow_light = 2
 RED()
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    control.waitMicros(10)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+    Sensor()
     if (distance < 5) {
-        Counter += 1
-    } else if (Counter == 4) {
         trafficlightreal()
+    } else if (Counter < 3) {
+        phantom()
     }
 })
